7428e318d1b4c1e98dffe025fb458be47bb381fe
Tushar Chhipekar <tusharchhipekar21@gmail.com>
2026-07-11 05:29:37 +0530

    feat: initialize frontend application with React and Vite configuration

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import { config } from "./configs/config";
 import { authRoutes } from "./routes/auth.route";
+import { exchangeRouter } from "./routes/exchange.route";
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/exchange", exchangeRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port http://localhost:${config.PORT}`);
